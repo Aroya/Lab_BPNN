285678f1443cf18413f1d420e5f3451bf678d42d
Aroya <aroya@sina.com>
2017-12-13 22:45:16 +0800

    finalv0.0a

--- BPNN/Source.cpp
+++ BPNN/Source.cpp
@@ -11,15 +11,15 @@ void main() {
 	//Divider("dfl.csv");
 	//分隔符换为空格
 	int trainCols = transform("train.csv","tra.csv");
-	int valiCols = transform("validation.csv", "vali.csv");
-	//int testCols = transform("dfl2.csv", "test.csv");
+	//int valiCols = transform("validation.csv", "vali.csv");
+	int testCols = transform("dfl2.csv", "test.csv");
 
 	Reader train("tra.csv", trainCols);
 	train.normalization(1);
-	Reader vali("vali.csv", valiCols);
-	vali.normalization(1);
-	//Reader test("test.csv", testCols);
-	//test.normalization(1);
+	//Reader vali("vali.csv", valiCols);
+	//vali.normalization(1);
+	Reader test("test.csv", testCols);
+	test.normalization(1);
 
 	double **trainData = new double*[train.rows];
 	for (int i = 0; i < train.rows; i++) trainData[i] = train.data[i];
@@ -28,15 +28,15 @@ void main() {
 		trainFlag[i] = new double[1] {double(train.flag[i])};
 	}
 
-	double **valiData = new double*[vali.rows];
-	for (int i = 0; i < vali.rows; i++) valiData[i] = vali.data[i];
-	double **valiFlag = new double*[vali.flag.size()];
-	for (int i = 0; i < vali.flag.size(); i++) {
-		valiFlag[i] = new double[1]{ double(vali.flag[i]) };
-	}
+	//double **valiData = new double*[vali.rows];
+	//for (int i = 0; i < vali.rows; i++) valiData[i] = vali.data[i];
+	//double **valiFlag = new double*[vali.flag.size()];
+	//for (int i = 0; i < vali.flag.size(); i++) {
+	//	valiFlag[i] = new double[1]{ double(vali.flag[i]) };
+	//}
 
-	//double **testData = new double*[test.rows];
-	//for (int i = 0; i < test.rows; i++) testData[i] = test.data[i];
+	double **testData = new double*[test.rows];
+	for (int i = 0; i < test.rows; i++) testData[i] = test.data[i];
 
 	int layers = 3;
 	int *layerNodes = new int[layers-1] {trainCols-1, 1};
@@ -46,12 +46,13 @@ void main() {
 	bpnn.setInputNodes(trainCols);
 	bpnn.setLayerNodes(layerNodes);
 	
-	for (int i = 0; i < 4000; i++) {
+	for (int i = 0; i < 200; i++) {
 		//bpnn.runGroup(trainData, trainFlag, train.rows, defaultActive, defaultActiveD);
-		bpnn.runGroup(trainData, trainFlag, train.rows, sigmoid, sigmoidD);
+		bpnn.runGroup(trainData, trainFlag, train.rows, sigmoid, sigmoidD);//默认参数为1，输入验证结果并学习
+		//bpnn.runGroup(valiData, valiFlag, vali.rows, sigmoid, sigmoidD,-1);//<0只验证结果
 	}
-	bpnn.runGroup(valiData, valiFlag, vali.rows, sigmoid, sigmoidD,0);
-	//bpnn.runGroup(testData, nullptr, test.rows, sigmoid, sigmoidD, 0);
+	
+	bpnn.runGroup(testData, nullptr, test.rows, sigmoid, sigmoidD, 0);//=0结果写入文件
 	//bpnn.runGroup(trainData, trainFlag, train.rows, sigmoid, sigmoidD,0);
 
 	//last line
